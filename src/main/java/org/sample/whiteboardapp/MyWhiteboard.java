175446eabc0e8d0862475d02bceb16b1f8c5d5bb
Paulo Gonçalves <paulogoncalves@recife.ifpe.edu.br>
2022-10-14 11:22:47 -0300

    Simplif

--- src/main/java/org/sample/whiteboardapp/MyWhiteboard.java
+++ src/main/java/org/sample/whiteboardapp/MyWhiteboard.java
@@ -2,35 +2,20 @@ package org.sample.whiteboardapp;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+
 import jakarta.websocket.EncodeException;
-import jakarta.websocket.OnClose;
 import jakarta.websocket.OnMessage;
-import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
 
+// @ServerEndpoint(value = "/whiteboardendpoint")
 @ServerEndpoint(value = "/whiteboardendpoint", encoders = { FigureEncoder.class }, decoders = { FigureDecoder.class })
 public class MyWhiteboard {
-
-    private static Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
-
-    @OnOpen
-    public void onOpen(Session peer) {
-        peers.add(peer);
-    }
-
-    @OnClose
-    public void onClose(Session peer) {
-        peers.remove(peer);
-    }
 
     @OnMessage
     public void broadcastFigure(Figure figure, Session session) throws IOException, EncodeException {
         System.out.println("broadcastFigure: " + figure);
-        for (Session peer : peers) {
+        for (Session peer : session.getOpenSessions()) {
             if (!peer.equals(session)) {
                 peer.getBasicRemote().sendObject(figure);
             }
@@ -40,7 +25,7 @@ public class MyWhiteboard {
     @OnMessage
     public void broadcastSnapshot(ByteBuffer data, Session session) throws IOException {
         System.out.println("broadcastBinary: " + data);
-        for (Session peer : peers) {
+        for (Session peer : session.getOpenSessions()) {
             if (!peer.equals(session)) {
                 peer.getBasicRemote().sendBinary(data);
             }
